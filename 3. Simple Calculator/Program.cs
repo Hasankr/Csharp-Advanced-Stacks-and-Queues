4a69392a29b78cf02ee519f4bc074a8466ee1b08
hasankr <hasankr@abv.bg>
2022-09-15 23:20:41 +0300

    added 3

---
+++ 3. Simple Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] expressions = Console.ReadLine().Split();
+            Stack<string> stack = new Stack<string>();
+            for (int i = 0; i < expressions.Length; i++)
+            {
+              
+                stack.Push(expressions[i]);
+                if (stack.Count == 3)
+                {
+                    int first = int.Parse(stack.Pop());
+                    var sign = stack.Pop();
+                    int second = int.Parse(stack.Pop());
+                    int result = 0;
+                    if (sign == "+")
+                    {
+                        result = first + second;
+                    }
+                    if (sign == "-")
+                    {
+                        result = second - first;
+                    }
+                    stack.Push(result.ToString());
+                }
+            }
+            Console.WriteLine(stack.Pop());
+        } 
+    }
+
+}
