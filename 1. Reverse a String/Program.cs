39b88fd047bc4a83fab04bcc0eeee0c684ad223a
hasankr <hasankr@abv.bg>
2022-09-15 21:21:49 +0300

    Add project files.

---
+++ 1. Reverse a String/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Reverse_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            foreach (char item in input)
+            {
+                stack.Push(item);
+            }
+            foreach (var item in stack)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
